ece3f91f84cac60210556434303785b101b13509
viet.vo <viethvo@kms-technology.com>
2018-11-10 22:00:33 +0700

    bz9 | Fix "Security Misconfiguration"

---
+++ SecurityCoding/SecurityCoding/Controllers/ErrorController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecurityCoding.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("Index");
+        }
+        public ViewResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View("NotFound");
+        }
+
+        public ViewResult Forbidden()
+        {
+            Response.StatusCode = 403;
+            return View("Forbidden");
+        }
+
+        public ViewResult ServerError()
+        {
+            Response.StatusCode = 500;
+            return View("ServerError");
+        }
+    }
+}
